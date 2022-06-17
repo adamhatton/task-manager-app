b5631d628c5c49cc740d1f0375679d1f31950a10
adamhatton <adamdhatton@gmail.com>
2022-06-17 19:05:10 +0000

    Add tasks read and update functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,26 @@
+// sidenav initialisation
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+// datepicker initialisation
+  document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+  });
+
+// select initialisation
+document.addEventListener('DOMContentLoaded', function() {
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
+
+// collapsible initialisation
+document.addEventListener('DOMContentLoaded', function() {
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
